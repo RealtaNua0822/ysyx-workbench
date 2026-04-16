969ef1b0c6253bb92cb5ecbb46aaf8d593cfdcb1
Realtanua <yankun924@gmail.com>
2026-04-16 20:18:54 +0800

    NJU-ProjectN/nemu ics2024 initialized
    NJU-ProjectN/nemu 8e7a0fecc95b5b5d2c1f6be0ec1b703da93356c0 Makefile: detect empty variables in find command

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
+// csrc/main.cpp
+#include <Vtop.h>
+#include <verilated.h>
 #include <stdio.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv) {
+    VerilatedContext* const contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* const top = new Vtop{contextp};
+
+    // 仿真逻辑...
+    while (!contextp->gotFinish()) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    }
+
+    delete top;
+    delete contextp;
+    return 0;
 }
