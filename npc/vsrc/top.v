8d4f0654606424c3f30c3f9c7f5ce291ee54fab9
RealtaNua <yankun924@gmail.com>
2026-04-21 15:41:34 +0800

    flow_led

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,9 +1,24 @@
 module top(
-  input a,
-  input b,
   input clk,
   input rst,
-  output f
+  output reg [15:0] ledr
 );
-  assign f = a ^ b;
+  reg [31:0] count; // 增加一个 32 位计数器
+  reg [3:0]  led_idx;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      count <= 0;
+      led_idx <= 0;
+      ledr <= 16'h1;
+    end else begin
+      if (count == 5000000) begin // 调这个数字，越大灯闪得越慢
+        count <= 0;
+        led_idx <= led_idx + 1;
+        ledr <= (16'h1 << led_idx); 
+      end else begin
+        count <= count + 1;
+      end
+    end
+  end
 endmodule
